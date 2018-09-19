46298609ef9e85d36caf1657e61090d2d82a23bb
Chad Lagomarsino <chadlagomarsino74@gmail.com>
2018-09-18 17:07:23 -0700

    added project page

--- server.js
+++ server.js
@@ -62,6 +62,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page'
+  })
+})
+
 //test bad route
 app.get('/bad', (req, res) => {
   res.send({
